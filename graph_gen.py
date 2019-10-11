c870b07c09af4e05b585415c6e67b801ae3a16a8
James Lyons <james@MacBook-Air-369.local>
2019-10-11 18:04:01 -0400

    Binary search, improved runtime complexity from O(N) to O(logN) for determining new state

--- graph_gen.py
+++ graph_gen.py
@@ -1,56 +1,48 @@
 from collections import defaultdict
 import sqlite3
-from markov_chain import Markov_Chain
-import random
-
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
-
-
-graph = {} # Word to next word/probability
-c.execute("SELECT * FROM epithets")
-rows = c.fetchall()
-def get_counts():    
+from collections import defaultdict
+from markov_chain import M_Chain
+NUMBER_OF_EPITHETS = 30
+MAX_CHAIN_LENGTH = 25
+def make_table(rows):
+    table = defaultdict(lambda: defaultdict(int))    
     for row in rows:
         epithet = row[0].split(" ")
         for i in range(len(epithet) - 1):
-            word1 = epithet[i]
-            word1.lower()
-            word2 = epithet[i + 1]
-            word2.lower()
-            if not word1 in graph:
-                graph[word1] = {}
-            if word2 in graph[word1]:
-                graph[word1][word2] += 1
-            else:
-                graph[word1][word2] = 1
-def get_probabilities():
-    for word1 in graph:
-    	summ = 0
-    	for word2 in graph[word1]:
-    		summ += graph[word1][word2]
-    	for word2 in graph[word1]:
-    		graph[word1][word2] = graph[word1][word2] / summ
-get_counts()
-get_probabilities()
+            word1 = epithet[i].lower()
+            word2 = epithet[i + 1].lower()
+            table[word1][word2] += 1
+    return table
 
-generated = 'The'
-r = random.random()
-if r >= 0.5:
-    generated = random.choice(list(graph))
-m_chain = Markov_Chain(graph, generated)
-for i in range(30):
-    for x in range(30):
-        nextt = m_chain.step()
-        if nextt == "": # Absorbing state
-            print(generated)
-            generated = 'The'
-            r = random.random()
-            if r >= 0.5:
-                generated = random.choice(list(graph))
-            m_chain.state = generated
-            break
-        generated += " "+nextt
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+c.execute("SELECT * FROM epithets")
+rows = c.fetchall()
+ # Word to next word/probability
+table = make_table(rows)
+m_chain = M_Chain(table)
+for i in range(NUMBER_OF_EPITHETS):
+    print(m_chain.generate_epithet(MAX_CHAIN_LENGTH))
+
+# generated = ['the']
+# r = random.random()
+# if r >= 0.5:
+#     generated[0] = random.choice(list(graph))
+# m_chain = Markov_Chain(graph, generated[0])
+# for i in range(30):
+#     for x in range(30):
+#         nextt = m_chain.step()
+#         if nextt == "": # Absorbing state
+#             # print((' '.join(generated)).title())
+#             generated.clear()
+#             generated.append('the')
+#             r = random.random()
+#             if r >= 0.5:
+#                 generated[0] = random.choice(list(graph))
+#             m_chain.state = generated[0]
+#             break
+#         # generated += " "+nextt
+#         generated.append(nextt)
         
 
 
--- markov_chain.py
+++ markov_chain.py
@@ -1,17 +1,53 @@
 import random
+from bisect import bisect
+from collections import defaultdict
+class M_Chain:
+    def __init__(self, table):
+        self.table = table
+        self.prefix_sums = defaultdict(list) # Prefix sum
+        self.p_no_name = defaultdict(list)
+        for word1 in self.table:
+            summ = 0
+            for word2 in self.table[word1]:
+                summ += self.table[word1][word2]
+                self.prefix_sums[word1].append(summ)
+                self.p_no_name[word1].append(word2)
 
-class Markov_Chain:
-	def __init__(self, table, start_state):
-		self.table = table
-		self.state = start_state
-	def step(self):
-		r = random.random()
-		s = 0
-		if self.state not in self.table: # Absorbing State - Impossible to leave
-			return ""
-		for word2 in self.table[self.state]:
-			s += self.table[self.state][word2]
-			if r <= s:
-				self.state = word2
-				return word2
-		return ""
+        # print(self.prefix_sums['grand'])
+        # print(self.table['grand'])
+    def generate_epithet(self, chain_length):
+        r = random.random()
+        epithet = ['the'] if r < 0.5 else [random.choice(list(self.table))]
+        for i in range(chain_length-1):
+            if len(self.prefix_sums[epithet[-1]]) == 0:
+                break
+            last = len(self.prefix_sums[epithet[-1]]) - 1
+            tot = self.prefix_sums[epithet[-1]][last]
+            val = random.randint(0, tot - 1)
+            ind = bisect(self.prefix_sums[epithet[-1]], val)
+            epithet.append(self.p_no_name[epithet[-1]][ind])
+        return ' '.join(epithet).title()
+        # if nextt == "": # Absorbing state
+        #     # print((' '.join(generated)).title())
+        #     generated.clear()
+        #     generated.append('the')
+        #     r = random.random()
+        #     if r >= 0.5:
+        #         generated[0] = random.choice(list(graph))
+        #     m_chain.state = generated[0]
+        #     break
+        # # generated += " "+nextt
+        # generated.append(nextt)
+
+    def step(self):
+        pass
+        # r = random.random()
+        # s = 0
+        # if self.state not in self.table: # Absorbing State - Impossible to leave
+        # 	return ""
+        # for word2 in self.table[self.state]:
+        # 	s += self.table[self.state][word2]
+        # 	if r <= s:
+        # 		self.state = word2
+        # 		return word2
+        # return ""
